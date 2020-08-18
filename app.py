a4bf42acdfa7a9b3bb7fc33caf446babecf8c6a7
rimurun <rimuru930@gmail.com>
2020-08-19 05:22:22 +0900

    生成した数字に対して条件式ごとに違う結果を返す関数を作成した｡その関数を利用して､結果を出力する処理を追加した｡

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import random
 
+
+def draw_oracle(num):
+    if not num % 2 == 0 and num >= 90:
+        output = '大吉'
+
+    if num % 2 == 0 and num <= 10:
+        output = '大凶'
+
+    if not num % 2 == 0:
+        output = '吉'
+
+    if num % 2 == 0:
+        output = '凶'
+
+    return output
+
+
 num = random.randint(1, 100)
+
+print(draw_oracle(num))
